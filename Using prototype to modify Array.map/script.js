c24cb203f439559c7e75f7f3dcdadb378918a85c
mackinnonj <102379816+mackinnonj@users.noreply.github.com>
2022-10-24 19:00:51 -0700

    Update script.js

--- Using prototype to modify Array.map/script.js
+++ Using prototype to modify Array.map/script.js
@@ -1,4 +1,6 @@
 
+// Attention!! do not modify existing methods because it can mess up all your other code that uses this method
+
 // Modify .map() to print '🗺' at the end of each item.
 // Example: 
 //console.log([1,2,3].map())
